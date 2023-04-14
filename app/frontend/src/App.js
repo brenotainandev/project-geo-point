1fa3af6f38f2d9c1d97320af97d0394ff2c6e124
Breno Tainan <brenotaguiar@gmail.com>
2023-04-14 13:04:30 -0300

    setup inicial front end

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,11 +1,12 @@
-import { Routes, Route } from 'react-router-dom';
 import React from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import Login from './pages/Login';
 import './styles/app.css';
 
 function App() {
   return (
     <Routes>
+      <Route exact path="/" element={<Navigate replace to="/login" />} />
       <Route path="/login" element={ <Login /> } />
     </Routes>
   );
