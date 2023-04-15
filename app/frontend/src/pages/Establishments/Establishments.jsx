ceb48f509579aba6eaa5e1430a8a7e553b54e64c
Breno Tainan <brenotaguiar@gmail.com>
2023-04-15 01:24:33 -0300

    estudando  leaflet

--- app/frontend/src/pages/Establishments/Establishments.jsx
+++ app/frontend/src/pages/Establishments/Establishments.jsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import { useState } from 'react';
+import { MapContainer, TileLayer, Marker, Polygon } from 'react-leaflet';
 
-const Establishments = () => {
+import 'leaflet/dist/leaflet.css';
+import "./style.css";
+
+function Establishments() {
+  const [polygonCoords, setPolygonCoords] = useState([]);
+  const [markerCoords, setMarkerCoords] = useState([-14.8500, -40.8399]);
+
+  function handleMapClick(e) {
+    setMarkerCoords([e.latlng.lat, e.latlng.lng]);
+    setPolygonCoords([...polygonCoords, [e.latlng.lat, e.latlng.lng]]);
+  }
+
   return (
-    <>
-      <section className="user-establishments-area">
-        <h1>Hello World!!!</h1>
-      </section>
-    </>
+    <div className="map-container">
+      <MapContainer center={markerCoords} zoom={13} scrollWheelZoom={false} onClick={handleMapClick} style={{ height: '500px' }}>
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <Marker position={markerCoords} />
+        {polygonCoords.length > 0 && <Polygon positions={[...polygonCoords, polygonCoords[0]]} />}
+      </MapContainer>
+    </div>
   );
-};
+}
+
 
 export default Establishments;
